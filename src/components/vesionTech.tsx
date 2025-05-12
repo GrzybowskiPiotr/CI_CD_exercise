7f6a3ccd6abb44b0821aa3a806133480633ae98a
Piotr Grzybowski <grzybowski_p@hotmail.com>
2025-05-12 14:25:52 +0200

    fix: enhance VersionTech component layout for better readability

--- src/components/vesionTech.tsx
+++ src/components/vesionTech.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function VersionTech() {
   return <div>
-    <p>App Version:</p>
-    <p>{__APP_VERSION__}</p>
+    <p>App Version:<span>{__APP_VERSION__}</span></p>
+    <p>mode: <span>{import.meta.env.VITE_ENVIRONMENT}</span></p>
   </div>
 }
