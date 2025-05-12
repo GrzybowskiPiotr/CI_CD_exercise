58625a0f5ff46c78a91e3c5711962869599a8b33
Piotr Grzybowski <grzybowski_p@hotmail.com>
2025-05-12 13:58:22 +0200

    fix: update app version reference to use __APP_VERSION__ in App and VersionTech components

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { VersionTech } from './components/vesionTech';
 const App = () => {
+  console.log(__APP_VERSION__)
   return (
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
-        <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service - CI_CD_execise {import.meta.env.VITE__APP_VERSION__}</h1>
+        <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service - CI_CD_execise {__APP_VERSION__}</h1>
       </div>
       <Outlet />
       <VersionTech/>
--- src/components/vesionTech.tsx
+++ src/components/vesionTech.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 export function VersionTech() {
   return <div>
     <p>App Version:</p>
-    <p>{import.meta.env.APP_VERSION}</p>
+    <p>{__APP_VERSION__}</p>
   </div>
 }
