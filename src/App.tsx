ed8a7d5517c2a86157eda495ab3dbeb1990ea44b
Piotr Grzybowski <grzybowski_p@hotmail.com>
2025-05-12 13:21:40 +0200

    fix: correct environment variable reference for app version display

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
-        <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service - CI_CD_execise {import.meta.env.__APP_VERSION__}</h1>
+        <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service - CI_CD_execise {import.meta.env.VITE__APP_VERSION__}</h1>
       </div>
       <Outlet />
       <VersionTech/>
