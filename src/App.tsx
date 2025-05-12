e0304b4744cbe65e6582211b85829fb3418e9d07
Piotr Grzybowski <grzybowski_p@hotmail.com>
2025-05-12 13:17:05 +0200

    fix: update app version display in header and comment out VersionTech component

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ const App = () => {
   return (
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
-        <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service - CI_CD_execise Netlify only conditionaly vercel</h1>
+        <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service - CI_CD_execise {import.meta.env.__APP_VERSION__}</h1>
       </div>
       <Outlet />
-      <VersionTech/>
+      {/* <VersionTech/> */}
     </div>
   );
 };
