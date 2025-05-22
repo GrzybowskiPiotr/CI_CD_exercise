09bde5e8235589a0e65e1339b2cfe70b3b7c1469
Piotr Grzybowski <grzybowski_p@hotmail.com>
2025-05-22 09:04:31 +0200

    feat: Add AWS SDK and NCC dependencies
    feat: Implement performance monitoring in Characters page
    
    chore: Update Terraform lock file and add external provider
    
    feat: Configure S3 backend for Terraform state management
    
    feat: Create performance monitoring API Gateway and Lambda function
    
    feat: Add access log format for API Gateway
    
    feat: Set up CloudWatch logging and dashboard for performance metrics
    
    feat: Implement Lambda function for performance checks
    
    chore: Define variables for Lambda function configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,31 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.40.0"
     }
+
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.3.3"
+    }
   }
 
   required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket         = "ofe-terraform-state-bucket-pg"
+    key            = "state/terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
 }
 
 provider "aws" {
   region = "eu-central-1"
+}
+
+module "perf" {
+  source = "./modules/perf"
+
+  lambda_function_name = "website_perf_check"
+  lambda_source_dir    = "${path.root}/../lib/lambda-perf"
 }
