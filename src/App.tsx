3c732aef7f4da936ebd693751ea0a7565a9a363d
Piotr Grzybowski <grzybowski_p@hotmail.com>
2025-05-25 13:38:47 +0200

    fix: enhance console log messages for environment variables in App and Characters components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { VersionTech } from './components/vesionTech';
 const App = () => {
-  console.log(import.meta.env.VITE_ENVIRONMENT);
+  console.log("Środowisko VITE_ENVIROMENT" + import.meta.env.VITE_ENVIRONMENT);
   return (
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
